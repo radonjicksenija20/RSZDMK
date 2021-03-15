f45d71760e553f6d7f48b901630faa3803c520c6
unknown <radonjicksenija20@gmail.com>
2021-03-15 13:20:53 +0100

    Dodajemo funkciju delay u granu A

---
+++ Vjezba2/Zadatak2/main.c
@@ -0,0 +1,12 @@
+#include <avr/io.h>
+#include <util/delay.h>
+int main()
+{
+ DDRB |= 1 << 5; //PB5 je izlaz
+ while (1)
+ {
+ PORTB |= 1 << 5; //LED ON
+ _delay_ms(2000); //pauza 1s
+ PORTB &= ~(1 << 5); //LED OFF
+ _delay_ms(2000); //pauza 1s
+ }
